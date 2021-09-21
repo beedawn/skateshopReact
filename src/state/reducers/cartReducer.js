413c0c2c30dfe7dd53bd6e421d1cd06991163bea
bee-dmg <brandonschmersal@gmail.com>
2021-09-20 20:10:49 -0400

    More Cleanup

--- src/state/reducers/cartReducer.js
+++ src/state/reducers/cartReducer.js
@@ -6,23 +6,12 @@ const initialState = {
 const reducer = (state = initialState, action ) => {
     switch(action.type) {
         case "ADD_ITEM":
-            if (state.cart.includes(action.payload)){
-            //     console.log("pizza")
-            // console.log(action.payload );
-           
-            //  state.cart.map(product => {
-            //     //  console.log("Were doing it")
-            //     console.log("State")
-            //     console.log(state)
-            //      action.payload.name === product.name ? /*{...product, qty: product.qty+1 }*/ console.log(product): product
-            //      console.log(product.qty)
-            //  })
-            }
-            else {
+            if (!state.cart.includes(action.payload)){
                 return { 
                     ...state,
                     cart: [...state.cart, action.payload]};
             }
+          
         // case "DELETE_ITEM":
         //     return state - action.payload;
         case "UPDATE_QTY":
